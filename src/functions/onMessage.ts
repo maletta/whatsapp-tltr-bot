c95d97e1c7b84e1127f52eb58cf62710b43db733
maletta <mauricio_maletta@hotmail.comm>
2024-02-24 02:16:47 -0300

    answer mentioned message; create deploy files

--- src/functions/onMessage.ts
+++ src/functions/onMessage.ts
@@ -1,10 +1,20 @@
-import { WhatsAppClient } from 'src/WhatsAppClient';
 import { Message } from 'whatsapp-web.js';
 
 import { isMessageToBot } from './shared';
 
 async function onMessage(message: Message) {
   const isValidMessage = await isMessageToBot(message);
+
+  if (!isValidMessage) {
+    return;
+  }
+  const chat = await message.getChat();
+  const contact = await message.getContact();
+
+  await chat.sendMessage(`Respota @${contact.id.user}`, {
+    mentions: [contact],
+  });
+
   console.log('isValidMessage');
   console.log(isValidMessage);
 }
--- src/server.ts
+++ src/server.ts
@@ -7,8 +7,8 @@ dotenv.config();
 
 const bot = WhatsAppClient.getClient();
 
-bot.on('ready', () => console.log('asdasddsa'));
-bot.on('authenticated', () => console.log('autenticado'));
+bot.on('ready', () => console.log('client is ready!'));
+bot.on('authenticated', () => console.log('authenticated!'));
 bot.on('message', onMessage);
 
 bot.initialize();
