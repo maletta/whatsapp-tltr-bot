1fa8bdd3dbeeb2e93113a62659c987f97f2c677c
Maletta <mauricio_maletta@hotmail.com>
2024-03-01 18:01:07 -0300

    quote messages

--- src/functions/commands.ts
+++ src/functions/commands.ts
@@ -1,5 +1,14 @@
+/* eslint-disable no-underscore-dangle */
 import { IChat } from 'src/@types/augmentation';
 import { Message, Contact, Client } from 'whatsapp-web.js';
+
+async function getQuotedMessage(message: Message): Promise<Message | null> {
+  if (message.hasQuotedMsg) {
+    return message.getQuotedMessage();
+  }
+
+  return null;
+}
 
 export async function commandEveryOne(
   client: Client,
@@ -18,10 +27,15 @@ export async function commandEveryOne(
     });
   }
   const text = responseText.join('');
+  const quotedMessage = await getQuotedMessage(message);
 
   console.log('mentions', mentions);
   console.log('responseText', text);
-  await client.sendMessage(message.from, 'Hello', {
+
+  await client.sendMessage(message.from, 'Vejam todos', {
     mentions: mentions as unknown as Contact[],
+    quotedMessageId: quotedMessage
+      ? quotedMessage.id._serialized
+      : message.id._serialized,
   });
 }
