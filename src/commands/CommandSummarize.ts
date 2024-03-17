b9982f31294253ae8fad8ed62bb8d8bf9aae2871
maletta <mauricio_maletta@hotmail.comm>
2024-03-17 03:18:14 -0300

    serate summarize responsabilities into two functions; fix duplicated responses on summarize command

--- src/commands/CommandSummarize.ts
+++ src/commands/CommandSummarize.ts
@@ -1,5 +1,6 @@
 import { GroupManager } from '@model/GroupManager';
-import { EnumTimeLimit, TimeLimit } from '@model/TimeLimit';
+import { Summary } from '@model/Summary';
+import { EnumTimeLimit, TimeLimit, TimeLimitOption } from '@model/TimeLimit';
 import { ITextSummarize } from '@services/ITextSummarize';
 import { TransformMessages } from '@utils/Formatters/TransformMessage';
 import { Chat, Client, Message, MessageTypes } from 'whatsapp-web.js';
@@ -23,7 +24,6 @@ class CommandSummarize implements ICommand {
     console.log('args ', args);
     console.log('message ', message.body);
 
-    console.log('from ', message.from);
     const summary = this.groups.getSummaryById(message.from, timeLimit);
     const haveValidSummary = summary && summary.isValid();
 
@@ -37,8 +37,31 @@ class CommandSummarize implements ICommand {
           timeLimit,
         ),
       );
+
+      return;
     }
 
+    try {
+      const newSummary = await this.createSummary(message, timeLimit);
+
+      message.reply(
+        this.formatSummaryResponse(
+          newSummary.content,
+          newSummary.formatCreatedAt(),
+          newSummary.formatExpiration(),
+          timeLimit,
+        ),
+      );
+    } catch (error) {
+      console.log('Error on create new Summary');
+      console.log(error);
+    }
+  }
+
+  private async createSummary(
+    message: Message,
+    timeLimit: TimeLimitOption,
+  ): Promise<Summary> {
     const chat = await message.getChat();
     const filteredMessages = await this.filterMessagesByHour(
       chat,
@@ -57,7 +80,8 @@ class CommandSummarize implements ICommand {
 
     console.log('messagesByTokenLimit', messagesByTokenLimit.length);
 
-    const promptMock = `Organize detalhadamente os tópicos abrangidos nessas conversas:`;
+    const promptMock = `Aqui está uma lista de mensagens trocadas por usuários que estão separadas por ';'.
+    Organize e detalhe os assuntos abrangidos nessas conversas:`;
 
     const messagesToResponse: string[] = [];
 
@@ -85,28 +109,19 @@ class CommandSummarize implements ICommand {
 
     const newSummary = summaryManager.getSummaryById(timeLimit);
 
-    message.reply(
-      this.formatSummaryResponse(
-        newSummary.content,
-        newSummary.formatCreatedAt(),
-        newSummary.formatExpiration(),
-        timeLimit,
-      ),
-    );
+    return newSummary;
   }
 
-  // get the number args for command summarize
-  // 30m, 1hr, 2hr, 4hr, 6hr
-  public getSummarizeTimeFromCommand(
-    args: string[],
-  ): keyof typeof EnumTimeLimit {
-    if (args.includes('1')) return '1_HOUR';
+  // get the number args for command summarize 30m, 1hr, 2hr, 4hr, 6hr
+  public getSummarizeTimeFromCommand(args: string[]): TimeLimitOption {
+    const firstArs = args.length > 0 ? args[0] : '';
+    if (firstArs.includes('1')) return '1_HOUR';
 
-    if (args.includes('2')) return '2_HOURS';
+    if (firstArs.includes('2')) return '2_HOURS';
 
-    if (args.includes('4')) return '4_HOURS';
+    if (firstArs.includes('4')) return '4_HOURS';
 
-    if (args.includes('6')) return '6_HOURS';
+    if (firstArs.includes('6')) return '6_HOURS';
 
     return '30_MINUTES';
   }
@@ -116,7 +131,6 @@ class CommandSummarize implements ICommand {
     timeLimit: EnumTimeLimit,
     limit: number = 1000,
   ): Promise<Message[]> {
-    console.log('filterMessagesByHour ', 'limit ', limit);
     const allMessages: Message[] = await chat
       .fetchMessages({
         limit,
@@ -143,7 +157,7 @@ class CommandSummarize implements ICommand {
       `Criado em: *${createdAt}*` +
       `\nPróxima atualização: *${expiresIn}*` +
       `\nTipo de resumo: *${EnumTimeLimit[timeLimit]}*` +
-      `\n*Resumo:*\n> ${summary}`
+      `\n\n*Resumo:*\n> ${summary}`
     );
   }
 }
--- src/utils/Formatters/TransformMessage.ts
+++ src/utils/Formatters/TransformMessage.ts
@@ -1,37 +1,6 @@
 import { Message } from 'whatsapp-web.js';
 
 class TransformMessages {
-  public static concat(
-    mensagens: Message[],
-    { maxTokens = 10000 } = {},
-  ): string[] {
-    const mensagensConcatenadas = [];
-    let strAtual = '';
-
-    // eslint-disable-next-line no-plusplus
-    for (let i = 0; i < mensagens.length; i++) {
-      const msg = mensagens[i].body; // Supondo que 'body' é a propriedade que contém o texto da mensagem
-
-      // Verifica se a adição da próxima mensagem excederá o limite de 3800 caracteres
-      if (strAtual.length + msg.length > maxTokens) {
-        // Se exceder, adiciona a string atual ao array de mensagens concatenadas
-        mensagensConcatenadas.push(strAtual);
-        // Começa uma nova string com a mensagem atual
-        strAtual = msg;
-      } else {
-        // Se não exceder, adiciona a mensagem à string atual
-        strAtual += msg;
-      }
-    }
-
-    // Adiciona a última string ao array de mensagens concatenadas
-    if (strAtual !== '') {
-      mensagensConcatenadas.push(strAtual);
-    }
-
-    return mensagensConcatenadas;
-  }
-
   public static createBatchOfMessages(
     messages: Message[],
     { maxTokens = 10000 } = {},
@@ -46,12 +15,12 @@ class TransformMessages {
       // Verifica se a adição da próxima mensagem excederá o limite de 3800 caracteres
       if (currentString.length + msg.length > maxTokens) {
         // Se exceder, adiciona a string atual ao array de mensagens concatenadas
-        messagesBatches.push(currentString);
+        messagesBatches.push(`${currentString}`);
         // Começa uma nova string com a mensagem atual
         currentString = msg;
       } else {
         // Se não exceder, adiciona a mensagem à string atual
-        currentString += msg;
+        currentString += `;${msg}`;
       }
     }
 
