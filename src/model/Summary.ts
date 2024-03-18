999b70518ed8f2defc89bfe4f02531020ebe2c37
maletta <mauricio_maletta@hotmail.comm>
2024-03-18 00:36:45 -0300

    get default time from serve

--- src/model/Summary.ts
+++ src/model/Summary.ts
@@ -55,12 +55,12 @@ class Summary {
   };
 
   private formatDate = (date: Date): string => {
-    const dd = String(date.getUTCDate()).padStart(2, '0');
-    const mm = String(date.getUTCMonth() + 1).padStart(2, '0'); // Janeiro é 0!
-    const yy = String(date.getUTCFullYear()).slice(-2);
-    const hh = String(date.getUTCHours()).padStart(2, '0');
-    const min = String(date.getUTCMinutes()).padStart(2, '0');
-    const ss = String(date.getUTCSeconds()).padStart(2, '0');
+    const dd = String(date.getDate()).padStart(2, '0');
+    const mm = String(date.getMonth() + 1).padStart(2, '0'); // Janeiro é 0!
+    const yy = String(date.getFullYear()).slice(-2);
+    const hh = String(date.getHours()).padStart(2, '0');
+    const min = String(date.getMinutes()).padStart(2, '0');
+    const ss = String(date.getSeconds()).padStart(2, '0');
 
     return `${dd}-${mm}-${yy} ${hh}:${min}:${ss}`;
   };
