46be268d53d5697c78e3050b1c242c7f1abd2cbe
Maletta <mauricio_maletta@hotmail.com>
2024-03-19 17:00:28 -0300

    fix random messages prompts

--- src/commands/CommandRandomMessage.ts
+++ src/commands/CommandRandomMessage.ts
@@ -21,28 +21,31 @@ class CommandRandomMessage implements ICommand {
 
     const randomNumber = Math.floor(Math.random() * 5) + 1;
 
+    const promptBadResponse =
+      'Quero um bordão ou frase para um personagem de um livro. O contexto é que você é um personagem robo em um grupo de amigos e em uma conversa lhe foi solicitado para dizer algo. Mas você está cansado de responder e das falas das pessoas. Seja muito breve e grosseiro. Me mande somente uma frase para usar.';
+
+    const promptFlirtResponse =
+      "Quero um bordão ou frase para um personagem de um livro. O contexto é que você é um personagem robo em um grupo de amigos e em uma conversa lhe foi solicitado para dizer algo. Mas você sem querer corteja uma pessoa em que estava pensando. Seja muito breve e criativo. Me mande somente uma frase  que é a sua cantada';";
     let prompt;
 
     switch (randomNumber) {
       case 1:
-        prompt =
-          'O contexto é que você  é bot para um grupo do whatsapp e lhe foi solicitado para dizer algo aleatório. Mas você está cansado de responder e ler mensagens das pessoas. Seja muito breve e grosseiro.';
+        prompt = promptBadResponse;
         break;
       case 2:
         prompt = 'fale uma curiosidade aleatória sobre qualquer coisa';
         break;
       case 3:
-        prompt =
-          'O contexto é que você  é bot para um grupo do whatsapp e lhe foi solicitado para dizer algo aleatório. Mas você está cansado de responder e ler mensagens das pessoas. Seja muito breve e grosseiro.';
+        prompt = promptBadResponse;
         break;
       case 4:
-        prompt = 'mande um elogio criativo e  emotes se quiser e nada mais';
+        prompt = promptFlirtResponse;
         break;
       case 5:
         prompt = 'mande uma citação famosa e o author';
         break;
       default:
-        prompt = 'me manda algo aleatório e nada mais';
+        prompt = promptBadResponse;
     }
 
     const response = await this.textSummarize.summarize(prompt, '');
