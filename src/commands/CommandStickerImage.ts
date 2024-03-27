c04ef1ef68272f86a553f6d6aec4019ad4ebe32a
Maletta <mauricio_maletta@hotmail.com>
2024-03-27 12:57:27 -0300

    refactor: remove console.log; avoid downloadMedia by whatswebjs library. Rather then download the media, now the message is decrypted by a own function using the url from media

--- src/commands/CommandStickerImage.ts
+++ src/commands/CommandStickerImage.ts
@@ -1,4 +1,5 @@
 import { IMessageRaw } from 'common/RawMessageType';
+import { MimeTypesEnum } from 'enums/MimeTypes';
 import { decryptMedia } from 'utils/decryptMedia';
 import {
   Client,
@@ -17,92 +18,33 @@ class CommandStickerImage implements ICommand {
     message: Message,
   ): Promise<void> {
     console.log('Command Sticker Image - execute ');
-    console.log('args ', args);
-    console.log('message ', message.body);
-
-    console.log('----------quoted Message');
-
-    const quotedMessage =
-      (await message.getQuotedMessage()) as unknown as IMessageRaw;
-
-    console.dir(quotedMessage, { depth: null });
-
-    const test = await decryptMedia(quotedMessage);
-
-    console.log('test');
-
-    console.dir(test, { depth: null });
-
-    console.log('base64');
-
-    console.dir(test.toString('base64'), { depth: null });
-    const media = new MessageMedia(
-      'video/mp4',
-      test.toString('base64'),
-      undefined,
-      // eslint-disable-next-line no-underscore-dangle
-      quotedMessage._data.size,
-    );
-    // const media = await (quotedMessage as unknown as Message).downloadMedia();
-
-    message.reply(media, message.from, { sendMediaAsSticker: true });
-
-    console.dir(media, { depth: null });
-  }
-
-  async execute2(
-    args: string[],
-    client: Client,
-    message: Message,
-  ): Promise<void> {
-    console.log('Command Sticker Image - execute ');
-    console.log('args ', args);
     console.log('message ', message.body);
 
     const options: MessageSendOptions = { sendMediaAsSticker: true };
     const media = await this.selectMedia(message);
 
-    console.log('-----------', message);
     if (media !== null && media !== undefined) {
-      console.log('response media to sticker');
-      // console.log(media);
       message
         .reply(media, message.from, options)
-        .then((reponseMessage) => reponseMessage.react('😴')) // 😼
+        .then((reponseMessage) => reponseMessage.react('😼')) // 😼
         .catch((error) => {
           console.log('Error on transforme image into sticker');
           console.log(error);
         });
     } else {
-      message.reply('Marque imagem válida 😴'); // 😼
+      message.reply('Marque imagem válida 😼'); // 😼
     }
   }
 
   private selectMedia = async (
     message: Message,
   ): Promise<MessageMedia | null> => {
-    console.log('select media function');
+    let selectedMessage: Message = message;
     if (message.hasQuotedMsg) {
-      const quotedMessage = await message.getQuotedMessage();
-      console.dir(`----------- quoted `);
-      console.dir(quotedMessage, { depth: null });
-      console.log('downloaded quoted Media ');
-      const media = await this.getMedia(quotedMessage)
-        .then((response) => {
-          console.log('------------------------sucesso ao Get Media');
-          console.dir(response, { depth: null });
-          return response;
-        })
-        .catch((error) => {
-          console.log('error on download media ', error);
-          return null;
-        });
-      console.log(media);
-      return media;
+      selectedMessage = await message.getQuotedMessage();
     }
-    console.log('downloaded message media  ');
-    const media = await this.getMedia(message);
-    console.log(media);
+
+    const media = await this.getMedia(selectedMessage);
 
     return media;
   };
@@ -111,26 +53,40 @@ class CommandStickerImage implements ICommand {
     console.log('Get Media function');
     try {
       if (this.isValidType(message)) {
-        const media = await message.downloadMedia();
-        return media;
+        const mimetype = this.getMimeType(message);
+        const mediaBuffered = await decryptMedia(
+          message as unknown as IMessageRaw,
+        );
+
+        const messageMedia = new MessageMedia(
+          mimetype,
+          mediaBuffered.toString('base64'),
+        );
+
+        return messageMedia;
       }
       return null;
     } catch (error) {
-      console.log('Error on download media');
+      console.log('Error on decrypt and create media');
       console.log(error);
       return null;
     }
   }
 
   private isValidType = (message: Message): boolean => {
-    console.log('isValidType ', message.hasMedia, message.type, message.isGif);
     return (
-      // (message.hasMedia && message.type === MessageTypes.IMAGE) || message.isGif
       (message.hasMedia &&
         (message.type === MessageTypes.IMAGE ||
           message.type === MessageTypes.VIDEO)) ||
       message.isGif
     );
+  };
+
+  private getMimeType = (message: Message): MimeTypesEnum => {
+    if (message.type === MessageTypes.VIDEO) {
+      return MimeTypesEnum.VIDEO;
+    }
+    return MimeTypesEnum.PNG;
   };
 }
 
---
+++ src/enums/MimeTypes.ts
@@ -0,0 +1,8 @@
+enum MimeTypesEnum {
+  VIDEO = 'video/mp4',
+  GIF = 'image/gif',
+  JPEG = 'image/jpg',
+  PNG = 'image/png',
+}
+
+export { MimeTypesEnum };
