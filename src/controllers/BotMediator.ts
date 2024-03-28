a6d19cf24736271f111b0a8ae6a8b0a6c422be5a
Maletta <mauricio_maletta@hotmail.com>
2024-03-28 15:50:37 -0300

    ignore commands with prefix that are no commands

--- src/controllers/BotMediator.ts
+++ src/controllers/BotMediator.ts
@@ -100,21 +100,50 @@ class BotMediator {
       return [EnumValidCommands.RANDOM_MESSAGE, []];
     }
 
-    if (command.startsWith(this.prefix)) {
-      if (!this.isValidCommand(command)) {
-        return [EnumSystemCommands.INVALID, []];
-      }
-
-      return [command.slice(1).toLowerCase() as EnumValidCommands, rest];
+    if (this.willByPassCommand(command)) {
+      return [EnumSystemCommands.DO_NOTHING, []];
     }
 
-    return [EnumSystemCommands.DO_NOTHING, []];
+    if (command.startsWith(this.prefix) && !this.isValidCommand(command)) {
+      return [EnumSystemCommands.INVALID, []];
+    }
+
+    return [command.slice(1).toLowerCase() as EnumValidCommands, rest];
+
+    // if (command.startsWith(this.prefix)) {
+    //   if (!this.isValidCommand(command)) {
+    //     return [EnumSystemCommands.INVALID, []];
+    //   }
+
+    //   return [command.slice(1).toLowerCase() as EnumValidCommands, rest];
+    // }
+
+    // return [EnumSystemCommands.DO_NOTHING, []];
+  }
+
+  private willByPassCommand(command: string) {
+    const haveMultiplePrefix = command.startsWith(
+      `${this.prefix}${this.prefix}`,
+    );
+    const IsCommandOnlyPrefix = command === this.prefix;
+    console.log(
+      'willByPassCommand ',
+      command,
+      haveMultiplePrefix,
+      IsCommandOnlyPrefix,
+    );
+    if (haveMultiplePrefix || IsCommandOnlyPrefix) {
+      return true;
+    }
+
+    return false;
   }
 
   private isValidCommand(
     command: string,
   ): command is EnumValidCommands | EnumSystemCommands {
     const validation = (enumItem) => this.prefix + enumItem === command;
+
     return (
       Object.values(EnumValidCommands).some(validation) ||
       Object.values(EnumSystemCommands).some(validation)
